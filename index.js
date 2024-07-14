685de5642e0190d6a5b74ebe347e3337e50aac60
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-07-14 11:13:00 +0530

    cleaned up weather fetch request and implemented redis

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import express from "express";
 import axios from "axios";
+import { createClient } from "redis";
 import "dotenv/config";
 
 const app = express();
 const port = 3000;
 
+const client = await createClient()
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .connect();
+
 app.use(express.json());
-app.get("/", async (request, response) => {
+
+app.post("/", async (request, response) => {
+  const location = request.body.location.replace(" ", "");
+
   const weatherData = await axios.get(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=${process.env.WEATHER_API_KEY}`
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${process.env.WEATHER_API_KEY}`
   );
 
-  response.json(weatherData.data);
+  const weather = {
+    location: weatherData.data.resolvedAddress,
+    temperature: weatherData.data.currentConditions.temp,
+    descripiton: weatherData.data.description,
+  };
+
+  response.json(weather);
 });
 
 app.listen(port, () => console.log("server started"));
